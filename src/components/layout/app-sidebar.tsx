b3467148abdd0763a13da821d357215a5a16076d
José Lucas <jose_lucas77@outlook.com>
2026-07-21 23:36:11 -0300

    feat: enhance AppSidebar with active link detection and styling improvements

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -13,16 +13,23 @@ import {
 import { NAV } from "@/lib/navigation-data";
 import { Handbag } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const pathname = usePathname();
+
+  const isActive = (to: string) =>
+    to === "/" ? pathname === "/" : pathname.startsWith(to);
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader className="mb-6">
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton className="data-[slot=sidebar-menu-button]:p-1.5!">
-              <Handbag className="size-5! text-primary" />
-              <span className="text-base font-bold text-primary">ZELO</span>
+              <Handbag className="size-5! text-blue-700 dark:text-blue-600" />
+              <span className="text-base font-bold text-blue-700 dark:text-blue-600">
+                ZELO
+              </span>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -31,19 +38,25 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              {NAV.map((item) => (
-                <SidebarMenuItem key={item.to}>
-                  <SidebarMenuButton
-                    tooltip={item.label}
-                    render={
-                      <Link href={item.to}>
-                        {<item.icon />}
-                        <span>{item.label}</span>
-                      </Link>
-                    }
-                  ></SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {NAV.map((item) => {
+                const active = isActive(item.to);
+                return (
+                  <SidebarMenuItem className="pb-1" key={item.to}>
+                    <SidebarMenuButton
+                      tooltip={item.label}
+                      render={
+                        <Link href={item.to}>
+                          {<item.icon />}
+                          <span>{item.label}</span>
+                        </Link>
+                      }
+                      className={
+                        active ? "bg-primary text-primary-foreground" : ""
+                      }
+                    ></SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
